8ad4274ee5141b501e25620b122ca82238e4ad5c
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-15 18:30:26 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,5 +18,20 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
-end
+  nds = directors_database
+  directors_total = {}
+  row_index = 0
+  while row_index < nds.length do
+    name = nds[row_index][:name]
+    directors_total[name] = 0
+    movies_list = nds[row_index][:movies]
+    column_index = 0
+    while column_index < movies_list.length do
+      directors_total[name] += movies_list[column_index][:worldwide_gross]
+      column_index +=1
+    end
+    row_index += 1
+  end
+  return directors_total
+end 
+
